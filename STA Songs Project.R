d0af41ec6d884aafb5b9e6b71d23e1aa85e3c3b7
Morgan McKee <63424557+morganpmckee@users.noreply.github.com>
2020-04-26 18:24:27 -0500

    Add files via upload

---
+++ STA Songs Project.R
@@ -0,0 +1,23 @@
+music = read.csv('/Users/morganpmckee/OneDrive/2019 - 2020/STA 371/Project/top10s.csv')
+View(music)
+
+install.packages('dplyr')
+library('dplyr')
+
+
+length(music$artist)
+# 603 artists made it into the top
+
+# Create table of unique values of artists in the dataset and how many songs they have
+a <- table(music$artist)
+a
+max(a)
+
+# Katy Perry has the most top songs in this dataset
+
+
+# return shape
+dim(music)
+summary(music)
+
+
