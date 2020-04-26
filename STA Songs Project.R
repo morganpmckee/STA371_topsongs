a5048657ac887f72fab0ca86a6503b5eae0ae7b5
Morgan McKee <63424557+morganpmckee@users.noreply.github.com>
2020-04-26 18:44:46 -0500

    Update STA Songs Project.R

--- STA Songs Project.R
+++ STA Songs Project.R
@@ -45,4 +45,17 @@ cor(top10$bpm,top10$pop)
 
 # Noelia
 
-
+mean(top10s$bpm)
+#average duration in seconds
+mean(top10s$dur)
+#artists with numbers of charted songs 
+topartist = table(top10s$artist, useNA = 'ifany')
+#top genres
+topgenre = table(top10s$top.genre, useNA = 'ifany')
+#plots of top genres and artists 
+plot(as.table(topgenre))
+plot(as.table(topartist))
+#top 5 artists 
+tail(names(sort(table(top10s$artist))),5)
+#top5 genries 
+tail(names(sort(table(top10s$genre))),5)
