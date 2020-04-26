4ad36697914924407aaa28fb7d274febb4d8769e
trinity987 <64384623+trinity987@users.noreply.github.com>
2020-04-26 18:39:38 -0500

    Update STA Songs Project.R

--- STA Songs Project.R
+++ STA Songs Project.R
@@ -21,7 +21,38 @@ dim(music)
 summary(music)
 
 
-# trinity
+# trinity - my df name is tops
+#histograms of the variables
+hist(tops$bpm) 
+mean(tops$bpm) #average beats per minute is 118.5456 beats
+hist(tops$dnce)  
+hist(tops$nrgy)  
+hist(tops$dB)  # skewed left
+hist(tops$live) # skewed right
+hist(tops$val)
+hist(tops$dur) # skewed right
+mean(tops$dur) # average song is 3 minutes and 44 seconds
+hist(tops$acous) # skewed right
+hist(tops$spch) # skewed right
+hist(tops$pop) # skewed left
+#basic correlations
+plot(tops$dnce,tops$val) # theres a positive correlation between danceability and valance 
+plot(tops$bpm,tops$val)
+plot(tops$dur,tops$pop)
+plot(tops$val,tops$nrgy) # theres a positive correlation between valence and energy 
+tops$year= as.factor(tops$year)
+# ggplots
+library(ggplot2)
+year1= subset(tops, year<2015)
+year1=as.factor(year1)
+
+ggplot(data = tops, aes(x=tops$dur, y=tops$pop)) +
+  geom_point(alpha = 0.5, aes(color=tops$bpm, size=tops$dnce, shape=tops$year)) +
+  xlab('Duration') + ylab('Popularity')
+
+ggplot(data = tops, aes(x=tops$val, y=tops$nrgy)) +
+  geom_point(alpha = 0.5, aes(color=tops$live, size=tops$acous)) +
+  xlab('Valence') + ylab('Energy')
 
 
 # Mohammad
